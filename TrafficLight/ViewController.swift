8e8625fa2d64c62a86f448bd221c0f341a012880
IgorDenisiuk <73896271+IgorDenisiuk@users.noreply.github.com>
2020-12-02 17:59:39 +0300

    Add Actions

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var toggleButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        redLightView.alpha = 0.3
+        redLightView.layer.cornerRadius = 70
+        
+        yellowLightView.alpha = 0.3
+        yellowLightView.layer.cornerRadius = 70
+        
+        greenLightView.alpha = 0.3
+        greenLightView.layer.cornerRadius = 70
+        
+        toggleButton.layer.cornerRadius = 10
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func makeButtonAction(_ sender: Any) {
+        redLightView.isHidden = true
+        yellowLightView.isHidden = true
+        greenLightView.isHidden = true
+    
+        if redLightView.isHidden {
+            redLightView.isHidden = false
+            yellowLightView.isHidden = false
+            greenLightView.isHidden = false
+            redLightView.alpha = 1
+            yellowLightView.alpha = 1
+            greenLightView.alpha = 1
+            toggleButton.setTitle("NEXT", for: .normal)
+        }
+       
+    
+    
+    }
+    
 }
-
